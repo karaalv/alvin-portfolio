c2c9925bceeadbad0fd9f31e30f0c665119d2bb7
karaalv <alviinkaranjja@gmail.com>
2025-07-20 16:51:37 +0100

    Created New Project Layout - Common ProjectItem component for reusability - Updated Projects component to use new styles and structure - Removed outdated projects and assets

---
+++ components/projects/ProjectItem.tsx
@@ -0,0 +1,53 @@
+/**
+ * @description This component renders a project
+ * item with an image and description.
+ */
+
+import Image from "next/image"
+import { useResponsiveContext } from "@/contexts/ResponsiveContext";
+
+import styles from "@styles/projects/ProjectItem.module.css"
+
+export interface ProjectItemProps {
+    imageSrc: string;
+    header: string;
+    techStack: string;
+    description: string;
+    link: string;
+}
+
+export default function ProjectItem({
+    imageSrc,
+    header,
+    techStack,
+    description,
+    link
+}: ProjectItemProps) {
+    const { isMobile } = useResponsiveContext();
+    const imageSize = isMobile ? '250' : '300';
+
+    return (
+        <a 
+            className={styles.container}
+            href={link}
+            target='_blank'
+            rel='noopener noreferrer'
+        >
+            <Image
+                src={imageSrc}
+                alt={`${header} screenshot`}
+                className={styles.project_image}
+                width={imageSize}
+                height={imageSize}
+            />
+
+            <div className={styles.text_container}>
+                <div className={styles.header_container}>
+                    <p className={styles.header_text}>{header}</p>
+                    <p className={styles.tech_text}>{techStack}</p>
+                </div>
+                <p className={styles.description_text}>{description}</p>
+            </div>
+        </a>
+    )
+}
--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -3,26 +3,64 @@
  * a carousel of projects for the portfolio.
  */
 
-import Gatsby from '@components/projects/Gatsby'
+import ProjectItem, { ProjectItemProps } from '@components/projects/ProjectItem'
 import Spacing from '@components/common/Spacing'
-// import styles from '@/styles/projects/Projects.module.css'
+
+import styles from '@styles/projects/Projects.module.css'
 import fonts from '@/styles/common/Typography.module.css'
 
 
 export default function Projects(){
+
+    const agentProject: ProjectItemProps = {
+        imageSrc: '/assets/agent.png',
+        header: 'RAG based portfolio agent',
+        techStack: 'Python, AWS',
+        description: (
+            'RAG-based chat agent that uses personal data to ' +
+            'answer questions and generate tailored CVs using a ' +
+            'retrieval-augmented pipeline'
+        ),
+        link: 'https://github.com/username/Portfolio-Agent'
+    }
+
+    const gatsbyProject: ProjectItemProps = {
+        imageSrc: '/assets/gatsby.png',
+        header: 'Blockchain ticketing platform',
+        techStack: 'Typescript, React, Solidity',
+        description: (
+            'Ticket distribution mobile platform ' +
+            'leveraging blockchain technology to ' +
+            'facilitate a unique ticket authentication ' +
+            'algorithm for enhanced security'
+        ),
+        link: 'https://github.com/karaalv/Gatsby-Public'
+    }
+
+    const racingProject: ProjectItemProps = {
+        imageSrc: '/assets/racing.png',
+        header: 'Co-Chief Electrical Systems Engineer',
+        techStack: 'PCB Design, Electrical Engineering',
+        description: (
+            'Research and development of electronics onto a ' +
+            'third-generation electrical race vehicle, 9th overall in ' +
+            'Electrical Vehicle class at Formula Student UK 2024'
+        ),
+        link: 'https://www.linkedin.com/feed/update/urn:li:activity:7227297553308897282/'
+    }
+
     return(
         <section
             id='Projects'
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                width: '100%'
-            }}
+            className={styles.section}
         >   
             <p className={fonts.heading}>Projects</p>
             <Spacing size='small'/>
-            
-            <Gatsby/>
+            <div className={styles.container}>
+                <ProjectItem {...agentProject} />
+                <ProjectItem {...gatsbyProject} />
+                <ProjectItem {...racingProject} />
+            </div>
         </section>
     )
 }
