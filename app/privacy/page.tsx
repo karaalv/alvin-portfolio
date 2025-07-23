b12aae20d122ccd435fa6ab362f7ae39e86f6d81
karaalv <alviinkaranjja@gmail.com>
2025-07-23 18:35:42 +0100

    Created Privacy Page - Moved some common styles to RootLayout - Added Privacy Page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,16 @@
+/**
+ * @description Privacy policy page
+ * for the website. 
+ */
+
+import styles from '@styles/pages/PrivacyPage.module.css'
+import fonts from '@styles/common/Spacing.module.css'
+
+export default function PrivacyPage() {
+    return (
+        <div className={styles.privacy_page}>
+            <h1>Privacy Policy</h1>
+            <p>This is the privacy policy page for the website.</p>
+        </div>
+    )
+}
