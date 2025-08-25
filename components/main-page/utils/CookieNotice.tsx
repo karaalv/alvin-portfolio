5963edc9b7349316011afa618ad791f8675a5a0d
karaalv <alviinkaranjja@gmail.com>
2025-08-25 23:45:57 +0100

    Added Permanent Disable for Cookie Notice

--- components/main-page/utils/CookieNotice.tsx
+++ components/main-page/utils/CookieNotice.tsx
@@ -2,24 +2,42 @@
  * @description This component displays a
  * cookie notice to users.
  */
-import { toast } from 'react-hot-toast'
+import { useEffect, useState } from 'react'
 import { X } from 'lucide-react'
 
 import styles from '@styles/main-page/utils/CookieNotice.module.css'
 
 export default function CookieNotice() {
+    const [show, setShow] = useState(false)
+
+    useEffect(() => {
+        const hasAccepted = localStorage.getItem('cookie-accepted')
+        if (!hasAccepted) {
+            setShow(true)
+        }
+    }, [])
+
+    const handleAccept = () => {
+        localStorage.setItem('cookie-accepted', 'true')
+        setShow(false)
+    }
+
     return (
-        <div className={styles.container}>
-            <p className={styles.message}>
-                This website uses cookies to manage data.
-                For the best experience, avoid using private browsing.
-            </p>
-            <div 
-                className={styles.icon_container}
-                onClick={() => toast.dismiss()}
-            >
-                <X className={styles.icon} />
-            </div>
-        </div>
+        <>
+            {show && (
+                <div className={styles.container}>
+                    <p className={styles.message}>
+                        This website uses cookies to manage data.
+                        For the best experience, avoid using private browsing.
+                    </p>
+                    <div
+                        className={styles.icon_container}
+                        onClick={handleAccept}
+                    >
+                        <X className={styles.icon} />
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
